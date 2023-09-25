8ce16fa82dbd07929d8bfd2e815eb6a3a4e1a3ab
Anoushah <anoushah.wasif@emumba.com>
2023-09-25 11:32:08 +0500

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-const secretKey = crypto.randomBytes(32).toString('hex');
-console.log(secretKey);
-const date = new Date();
-// console.log(date);
-console.log(date.toString());
